40ccbbc3c0b7e43b0c84728867863c86c24d67b1
Takamon <nicolabirchall@gmail.com>
2016-09-19 22:02:19 +0100

    Edited colour scheme in item view. TODO: sort out star background colour

--- LogMyLife.Android/ItemViewActivity.cs
+++ LogMyLife.Android/ItemViewActivity.cs
@@ -14,7 +14,7 @@ using LogMyLife.Domain.Model;
 
 namespace LogMyLife.Android
 {
-    [Activity(Label = "ItemViewActivity")]
+    [Activity(Label = "", Icon = "@drawable/icon", Theme = "@style/CustomActionBarTheme")]
     public class ItemViewActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -29,6 +29,11 @@ namespace LogMyLife.Android
             ListView titleFieldList = FindViewById<ListView>(Resource.Id.titleFieldList);
             EditFieldAdapter adpaterFL = new EditFieldAdapter(this, ent.TitleData );
             titleFieldList.Adapter = adpaterFL;
+
+            //Set Rating bar star value
+            float d = 3;
+            RatingBar rb = (RatingBar)FindViewById(Resource.Id.ratingRBar);
+            rb.Rating = d;
 
             //populate otherFieldList
             ListView otherFieldList = FindViewById<ListView>(Resource.Id.otherFieldList);
